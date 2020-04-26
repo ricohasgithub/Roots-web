f94bd69f87017e98a26910caaf0923dac8dba05d
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-04-26 10:07:50 -0400

    Refactored Google Maps API Usage Code

--- public/js/community.js
+++ public/js/community.js
@@ -4,6 +4,24 @@ let navbar_username = document.getElementById("navbar-username");
 
 // The current user object
 let cUser;
+let cUserLocation;
+let walletid;
+
+let userLat;
+let userLong;
+
+if (!navigator.geolocation) {
+  console.log("Geolocation API not supported by browser");
+} else {
+  navigator.geolocation.getCurrentPosition(updateUserLoc);
+}
+
+function updateUserLoc (position) {
+  userLat = position.coords.latitude;
+  userLong = position.coords.longitude;
+  console.log(userLat);
+  console.log(userLong);
+}
 
 // Retrieve the cloud firestore object
 let db = firebase.firestore(app);
@@ -23,8 +41,11 @@ firebase.auth().onAuthStateChanged(function(user) {
   // Create a new document for the current user
   let cUserDocument = db.collection("users").doc(username);
 
-  // Append user information to the current document query
-  
+  // Retrieve user information from the current document query
+  cUserDocument.onSnapshot(function(doc) {
+      walletid = doc.data().wallet;
+      console.log(walletid);
+  });
 
 });
 
@@ -41,3 +62,22 @@ document.getElementById("logout").onclick = () => {
 }
 
 Radar.initialize("prj_live_pk_e26e9849721e36acebb7af3909c6985692c52e43");
+
+// Create the script tag, set the appropriate attributes
+var script = document.createElement("script");
+script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCmIzUERgQIiesJMiulSvn9ZNTq0GP6aV0&callback=initMap";
+script.defer = true;
+script.async = true;
+
+// Attach your callback function to the `window` object
+window.initMap = function () {
+
+  let map = new google.maps.Map(document.getElementById("map"), {
+      center: {lat: userLat, lng: userLong},
+      zoom: 12
+  });
+
+}
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
