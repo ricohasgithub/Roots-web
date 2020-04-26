93261c4b996668ab6c76bbe7030186aaee2ad0c1
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-04-25 22:40:55 -0400

    Added Blockstack Authentication

---
+++ public/js/localstack.js
@@ -0,0 +1,22 @@
+
+document.getElementById('signin-button').addEventListener('click', function() {
+   blockstack.redirectToSignIn("localhost:5000/community.html")
+})
+
+function showProfile(profile) {
+   var person = new blockstack.Person(profile)
+   document.getElementById('heading-name').innerHTML = person.name()
+   document.getElementById('avatar-image').setAttribute('src', person.avatarUrl())
+   document.getElementById('section-1').style.display = 'none'
+   document.getElementById('section-2').style.display = 'block'
+ }
+
+ if (blockstack.isUserSignedIn()) {
+  const userData = blockstack.loadUserData()
+   showProfile(userData.profile)
+ } else if (blockstack.isSignInPending()) {
+   blockstack.handlePendingSignIn()
+   .then(userData => {
+     showProfile(userData.profile)
+   })
+ }
